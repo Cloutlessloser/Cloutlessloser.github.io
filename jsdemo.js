3caef9b1c473e54e2497c236127e2bd21531cd18
Cloutlessloser <158795889+Cloutlessloser@users.noreply.github.com>
2026-06-30 14:53:29 -0400

    Update jsdemo.js

--- jsdemo.js
+++ jsdemo.js
@@ -1,8 +1,8 @@
 console.log("This is loaded from the external js");
 alert("This is an alert!");
-var name = "Lucas Gonzalez";
-var school = "UNCC";
-let hobby = "Playing Video Games";
+var name = prompt("What is your name?");
+var school = prompt("What school do you go to?");
+let hobby = prompt("What do you like to do?");
 console.log(`Name: ${name}`);
 console.log(`School: ${school}`);
 console.log(`Hobby: ${hobby}`);
