97eb8e945f1dfbf6aeba248a92cefdf0169435ae
Cloutlessloser <158795889+Cloutlessloser@users.noreply.github.com>
2026-07-09 14:17:09 -0400

    Update travelmap.js

--- travelmap.js
+++ travelmap.js
@@ -9,4 +9,6 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 var marker = L.marker(coordinates).bindPopup("<p>Hello, from Quebec City!</p>").addTo(map);
-var marker1 = L.circle(place1,{color: "black", fillColor: "#09c902", radius: 100}).addTo(map);
+var marker1 = L.circle(place1,{color: "black", fillColor: "#09c902", radius: 100}).bindPopup("<p>Hello, from Centre Municipal St Jean Ile D'Orléans!</p>").addTo(map);
+var marker2 = L.circle(place2,{color: "black", fillColor: "#09c902", radius: 100}).bindPopup("<p>Hello, from Montmorency Falls!</p>").addTo(map);
+var marker3 = L.circle(place3,{color: "black", fillColor: "#09c902", radius: 100}).bindPopup("<p>Hello, from Fairmont Le Château Frontenac!</p>").addTo(map);
