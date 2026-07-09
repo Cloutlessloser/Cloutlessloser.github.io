866f57c4a86f90a94b54ccf5ff065bb6860e91f5
Cloutlessloser <158795889+Cloutlessloser@users.noreply.github.com>
2026-07-09 14:08:35 -0400

    Update travelmap.js

--- travelmap.js
+++ travelmap.js
@@ -1,8 +1,12 @@
 // 46.8139° N latitude and -71.2080° W longitude
 var coordinates = [46.8139, -71.2080];
+var place1 = [46.917, -70.900];
+var place2 = [46.887508, -71.147911];
+var place3 = [46.811943, -71.205002];
 var map = L.map('map').setView(coordinates, 13);
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 var marker = L.marker(coordinates).bindPopup("<p>Hello, from Quebec City!</p>").addTo(map);
+var marker1 = L.circle(place1,{color: "black", fillColor: "#09c902", radius: 80}).addTo(map);
